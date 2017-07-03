49dcc0d0f472e9cf7c7bdd51a5e109737e0a732e
Double <fangmingdong0224@gmail.com>
2017-07-03 23:35:25 +0800

    init

--- Mp3Record/ViewController.swift
+++ Mp3Record/ViewController.swift
@@ -7,19 +7,139 @@
 //
 
 import UIKit
+import SnapKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let mBtnStart: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Start", for: .normal)
+        btn.setTitleColor(UIColor.black, for: .normal)
+        btn.addTarget(self, action: #selector(startRecord), for: .touchUpInside)
+        return btn
+    }()
+    
+    let mBtnStop: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Stop", for: .normal)
+        btn.setTitleColor(UIColor.black, for: .normal)
+        btn.addTarget(self, action: #selector(stopRecord), for: .touchUpInside)
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        AVAudioSession.sharedInstance().requestRecordPermission () {
+            [unowned self] allowed in
+            if allowed {
+                // Microphone allowed, do what you like!
+                NSLog("permission Allowed")
+                
+                self.startRecord()
+            } else {
+                // User denied microphone. Tell them off!
+                NSLog("permission deni")
+                // tell user turn on Audio record permission in settings
+                
+            }
+        }
+        
+        
+        
+        initView()
+        
     }
-
+    
+    func initView(){
+        self.view.addSubview(mBtnStart)
+        self.view.addSubview(mBtnStop)
+        
+        mBtnStart.snp.makeConstraints { (make) in
+            make.width.equalTo(self.view)
+            make.height.equalTo(20)
+            make.top.equalTo(self.view).offset(20 + 44)
+        }
+        mBtnStop.snp.makeConstraints { (make) in
+            make.width.equalTo(self.view)
+            make.height.equalTo(20)
+            make.top.equalTo(mBtnStart.snp.bottom)
+        }
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    var audioRec: AVAudioRecorder?
+    
+    
+    func startRecord(){
+        
+        // 0. Create a URL path where you’ll store the recording. This is usually the documents folder of your app.
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let docsDirect = paths[0]
+        
+        let fileName = 
+        
+        let audioUrl = try docsDirect.appendingPathComponent("audioFileName.m4a")
+        
+        
+        // 1. create the session
+        let session = AVAudioSession.sharedInstance()
+        
+        do {
+            // 2. configure the session for recording and playback
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+            try session.setActive(true)
+            // 3. set up a high-quality recording session
+            let settings = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 44100,
+                AVNumberOfChannelsKey: 2,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            // 4. create the audio recording, and assign ourselves as the delegate
+            audioRec = try AVAudioRecorder(url: audioUrl, settings: settings)
+            audioRec?.delegate = self
+            audioRec?.record()
+            
+            
+        }
+        catch let error {
+            // failed to record!
+        }
+        
+    }
+    
+    func stopRecord(){
+        audioRec?.stop()
+    }
+    
+    func recordingEnded(success: Bool){
+        // record end result
+        NSLog("Record End %@", String(success))
+    }
+    
+    
+}
 
-
+extension ViewController: AVAudioRecorderDelegate{
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            recordingEnded(success: false)
+        } else {
+            recordingEnded(success: true)
+        }
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,37 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Mp3Record' do
+
+    # Layout
+    pod 'SnapKit', '~> 3.0'
+
+    # Network and data
+    pod 'Kingfisher', '~> 3.0'
+    pod 'Moya/RxSwift'
+    pod 'ObjectMapper', '~> 2.2'
+    pod 'ReachabilitySwift'
+
+    # Rx
+    pod 'RxSwift', '~> 3.0'
+    pod 'RxCocoa', '~> 3.0'
+    pod 'RxDataSources', '~> 1.0'
+    
+    # view
+    # pod 'MJRefresh'
+    pod 'MBProgressHUD', '~> 1.0.0'
+    #  pod "InfiniteCollectionView"   
+    # pod 'XLPagerTabStrip', '~> 7.0'
+    # pod 'PageMenu'
+    # pod 'ScrollPageView', '~> 0.1.4'
+
+
+    # other
+    # pod 'NSLogger/Swift'
+    pod 'IQKeyboardManagerSwift'
+    
+    # umeng
+    pod 'UMengAnalytics-NO-IDFA'
+
+end
